9eb03a9ef797b21fdaf3a8ee126c8266d5c59577
Sang Soo Ra <nadang82@gmail.com>
2020-03-11 15:23:15 +0100

    finished yelp

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,50 @@
+class ReviewsController < ApplicationController
+  before_action :set_review, only: %i[show edit update destroy]
+  def index
+    @reviews = Review.all
+  end
+
+  def show; end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @review.update(review_params)
+      @restaurant = Restaurant.find(review_params[:restaurant_id])
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to reviews_path
+  end
+
+  private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating, :restaurant_id)
+  end
+end
